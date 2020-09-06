8ccc0a922b10bb532bcefa6e6ff6e5517433562c
masato <masato.20@outlook.com>
2020-09-06 12:34:11 +0900

    2-4 課題
    ご確認お願いいたします。

---
+++ 2-4/src/study/Study.java
@@ -0,0 +1,27 @@
+package study;
+
+// ① FruitsConstantsをインポートしなさい
+
+// ② Fruits.javaをインポートしなさい
+
+/**
+ *
+ * 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
+ *
+ * 問①〜問③まであります。
+ * 全て回答し、課題にある画像と同じ表示になるようにしてください。
+ *
+ * @author s.nanaumi
+ *
+ */
+public class Study {
+
+    public static void main(String[] args) {
+
+        /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
+         *     また、課題の画像と同じ表示になるように
+         *     printFruitsの引数にはFruitsConstantsから呼び出した値を設定してください。
+         */
+
+    }
+}
